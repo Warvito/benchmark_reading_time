e449a37266723e4d020ac4f936786516b9d5e352
Walter Hugo Lopez Pinaya <walhugolp@gmail.com>
2020-11-17 11:39:58 +0000

    Prepare runai benchmark

--- run_bechmark.sh
+++ run_bechmark.sh
@@ -2,13 +2,9 @@
 # Bechmark reading time using MONAI's different dataset types: Normal, Persistent and Cached.
 
 # Get docker image
-docker pull projectmonai/monai
+docker pull projectmonai/monai:0.3.0
 
 chmod +x $(pwd)/monai_benchmark.py
 
-mkdir ./temp
-docker run --volume $(pwd):/opt/monai/benchmark --env MONAI_DATA_DIRECTORY="/opt/monai/benchmark/temp/" projectmonai/monai:latest \
-  /opt/monai/benchmark/monai_benchmark.py \
-  | tee output.txt
-
-
+nvidia-docker run --volume $(pwd):/project --env OUTPUT_DIR="/project/outputs" projectmonai/monai:0.3.0 \
+  /project/monai_benchmark.py
